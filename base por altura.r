556a7c348731d91fb5e18d66adc4534f706c1589
Lyndon0199 <48296136+Lyndon0199@users.noreply.github.com>
2019-03-18 14:15:44 -0500

    Add files via upload

---
+++ base por altura.r
@@ -0,0 +1,8 @@
+FsuperfRect=function(){
+  cat("Longitud de la base: ");   
+    base=scan(n=1,quiet=TRUE) 
+    cat("Longitud de la altura: "); 
+    altura=scan(n=1,quiet=TRUE) 
+    S=base*altura 
+    cat("Superficie del rectángulo: ",S) 
+  }
